fa86383a513ab0662fb6dfdfe031cdb53c1a8e6e
Sanjay <136176879+sanjayy-k@users.noreply.github.com>
2025-12-12 22:41:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT 
+  port: process.env.DB_PORT || 3306
 })
 // console.log(process.env.DB_HOST);
 // console.log(process.env.DB_USER);
