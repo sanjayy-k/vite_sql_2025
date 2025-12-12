652c3d0c40e8fd61cfad6fb25ca5f643dd7cb9f7
Sanjay <136176879+sanjayy-k@users.noreply.github.com>
2025-12-12 23:18:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ import bodyParser from 'body-parser'
 
 const app = express();
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
